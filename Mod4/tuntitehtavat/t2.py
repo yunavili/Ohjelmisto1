96c5b0242075e6d5d9cf1444ee17bb723012190e
yunavili <ivanskayulia.yunavili@gmail.com>
2026-08-27 01:10:10 +0300

    tuntitehtava3 moduuli 4 'uusi tarina'

--- Mod4/tuntitehtavat/t2.py
+++ Mod4/tuntitehtavat/t2.py
@@ -4,20 +4,14 @@ try:
     user_height = int(input("Enter your height in centimeters: "))
     user_age = int(input("Enter your age in years: "))
 
-    if user_height >= 100 and user_age >= 8:
-        
-        if user_height >= 100 and user_height < 140:
-            print("You can go to children's rides.")
-        elif user_height >= 140 and user_height <= 195:
-            print("You can go to all rides.")
-        else: 
-            print("You can go to all children's rides except Tuliretki.")
-    
-    elif user_height < 140 and user_age < 8:
-                print("You can go to all children's rides except Tuliretki.")
-    
+    if user_age < 8 or user_height < 100:
+        print("You can't go to any of rides.")
+    elif user_height < 140:
+        print("You can go to children's rides including Kirnu but except Tuliretki.")
+    elif user_height < 195:
+        print("You can go to all rides.")
     else: 
-        print("You can't go to any of rides")
+        print("You can go to any of rides including Tuliretki but except Kirnu.")
     
 except ValueError:
-    print("Please enter a valid height in centimeters.")
+    print("Please enter valid numbers for height and age.")
--- Mod4/tuntitehtavat/t3.py
+++ Mod4/tuntitehtavat/t3.py
@@ -1,8 +1,43 @@
-print("This story will put you into the tale. You'll have to choose!")
+print("This story will put you into the tale. You'll have to choose for the sake of your life!")
 user_name = input("Tell us your name, traveller: ")
 
-weapon = ["knife", "wand"]
+print("\nChoose your weapon: ")
+print("1. A rubber duck that squeaks agressively")
+print("2. A stale baguette (surprisingly sharp)")
+print("Don't try anything else!")
 
-for object in weapon:
-    if "knife" in object:
-        print()
+weapon_choise = input("Enter 1 or 2: ").strip()
+
+weapon = ["rubber duck", "stale baguette", "a surprisingly heavy Spoon"]
+
+
+if weapon_choise == "1":
+    user_weapon = weapon[0]
+    battle_sound = "*SQUEAK SQUEAK*"
+    effect = "instantly deafening all enemies with high-pitched doom."
+elif weapon_choise == "2":
+    user_weapon = weapon[1]
+    battle_sound = "*CRUNCH*"
+    effect = "shattering the dark lord's shield like cheap glass."
+else:
+    user_weapon = weapon[2]
+    battle_sound = "*CLINK*"
+    effect = "leaving everyone mildly confused."
+
+
+story = f"""
+--- THE LEGEND OF {user_name.upper()} ---
+
+The ancient dragon roared, unleashing a wave of fury across the battlefield. 
+The mighty warrior {user_name} stepped forward, holding high their legendary arms: {user_weapon}!
+
+The dragon paused, looking utterly bewildered. 
+
+With a battle cry, {user_name} charged forward. {battle_sound}! 
+The power of {user_weapon} was unmatched, {effect}
+
+The dragon threw its hands up in surrender, bought {user_name} a coffee, 
+and they both lived weirdly ever after.
+"""
+
+print(story)
