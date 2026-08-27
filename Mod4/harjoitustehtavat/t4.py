f83f8de09ee5c20ef4055cb491958ae40ca4293e
yunavili <ivanskayulia.yunavili@gmail.com>
2026-08-28 01:44:51 +0300

    mod4 harjoitustehtävä t4 korjattu

--- Mod4/harjoitustehtavat/t4.py
+++ Mod4/harjoitustehtavat/t4.py
@@ -8,11 +8,15 @@ except ValueError:
     print("Invalid value. Please enter a number.")
     sys.exit()
 
-if input_year <= 0:
-    print("Invalid year. Please enter a valid number.")
+if len(str(input_year)) > 4:
+    print("Invalid input. Please enter a valid 1-4 digit number.")
     sys.exit()
 
-if input_year % 4 == 0 and input_year % 400 == 0 and input_year % 100 == 0:
+if input_year <= 0:
+    print("Invalid year. Please enter a valid number with a positive value.")
+    sys.exit()
+
+if input_year % 4 == 0 and (input_year % 100 != 0 or input_year % 400 == 0):
     print(f"{input_year} is a leap year, yeah!")
-else: 
+else:
     print("It's not a leap year, nope.")
